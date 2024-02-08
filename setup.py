a33e50d01e9c0bb5727197f070fcc18c79be34b4
Robert Swanson <rswanson3141@gmail.com>
2024-02-08 15:39:58 -0800

    debug

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ VAR_STORE.pull_ignore_categories.extend(pull_ignore_categories)
 
 host_dir = VAR_STORE.get_host_dir()
 should_overwrite = not host_dir.exists() or [p.name for p in host_dir.iterdir()] == ["var_store.json"]
+print([p.name for p in host_dir.iterdir()])
+
 if should_overwrite:
     copy_host_dir = VAR_STORE.get_hosts_dir() / copy_hostname
     host_dir = VAR_STORE.get_host_dir()
